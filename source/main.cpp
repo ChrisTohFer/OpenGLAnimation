ab7576c3eb85264a710227f00fd9d8c193039cd6
Chris <chrisrkemp@hotmail.co.uk>
2022-01-01 13:50:20 +0000

    Drawn triagnle

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,5 @@
+#include "shaders.h"
+
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
@@ -11,6 +13,90 @@ const static int size_y = 600;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+//helper functions
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+struct VBO
+{
+    unsigned int id;
+    VBO() {
+        static constexpr float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        };
+        glGenBuffers(1, &id);
+        glBindBuffer(GL_ARRAY_BUFFER, id);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    }
+};
+
+struct VShader
+{
+    unsigned int id;
+    VShader()
+    {
+        id = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(id, 1, &vertexShader, NULL);
+        glCompileShader(id);
+    }
+};
+struct FShader
+{
+    unsigned int id;
+    FShader()
+    {
+        id = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(id, 1, &fragmentShader, NULL);
+        glCompileShader(id);
+    }
+};
+struct ShaderProgram
+{
+    unsigned int id;
+    ShaderProgram(unsigned int v, unsigned int f)
+    {
+        id = glCreateProgram();
+        glAttachShader(id, v);
+        glAttachShader(id, f);
+        glLinkProgram(id);
+        glUseProgram(id);
+    }
+};
+struct VAO
+{
+    unsigned int id;
+    VAO()
+    {
+        glGenVertexArrays(1, &id);
+        glBindVertexArray(id);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+    }
+};
+
+void render()
+{
+    //One time setup
+    static VBO vbo;
+    static VShader vshade;
+    static FShader fshade;
+    static ShaderProgram program(vshade.id, fshade.id);
+    static VAO vao;
+
+    //Clear
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //Draw
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main()
@@ -40,9 +126,14 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        render();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glfwTerminate();
     return 0;
 }
