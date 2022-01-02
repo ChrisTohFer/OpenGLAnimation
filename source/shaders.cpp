ba4a10ee7be105e9b079da84d070030b3ac67c3c
Chris <chrisrkemp@hotmail.co.uk>
2022-01-02 10:52:10 +0000

    Added error logging to shaderprogram

--- source/shaders.cpp
+++ source/shaders.cpp
@@ -1,21 +1,50 @@
 #include "shaders.h"
 
 #include "glad/glad.h"
+#include <iostream>
 
 ShaderProgram::ShaderProgram(const char* vert, const char* frag)
 {
+    int success;
+    char log[512];
+    
     auto vert_id = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vert_id, 1, &vertex_shader, 0);
     glCompileShader(vert_id);
+    glGetShaderiv(vert_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vert_id, 512, nullptr, log);
+        std::cout << "Error compiling vertex shader:\n" << log << "\n";
+        glDeleteShader(vert_id);
+        return;
+    }
 
     auto frag_id = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(frag_id, 1, &fragment_shader, 0);
     glCompileShader(frag_id);
+    glGetShaderiv(frag_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(frag_id, 512, nullptr, log);
+        std::cout << "Error compiling fragment shader:\n" << log << "\n";
+        glDeleteShader(frag_id);
+        return;
+    }
 
     id = glCreateProgram();
     glAttachShader(id, vert_id);
     glAttachShader(id, frag_id);
     glLinkProgram(id);
+    glGetProgramiv(id, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(id, 512, nullptr, log);
+        std::cout << "Error linking shaderprogram:\n" << log << "\n";
+        glDeleteProgram(id);
+        return;
+    }
+
     glUseProgram(id);
 
     glDeleteShader(vert_id);
